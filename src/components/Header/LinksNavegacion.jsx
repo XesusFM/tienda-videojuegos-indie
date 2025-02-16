537527a89289d78b9a2ce7fffc41431099cba83e
Jesus <jesusfm1999@gmail.com>
2025-02-16 20:23:40 +0100

    ✨ feat: Añadir título "Tienda" en el componente de navegación y mejorar la estructura visual

--- src/components/Header/LinksNavegacion.jsx
+++ src/components/Header/LinksNavegacion.jsx
@@ -4,10 +4,9 @@ import { FaXbox, FaWindows } from "react-icons/fa";
 
 const LinksNavegacion = () => (
     <nav className="flex items-center gap-6 text-gray-300">
-        <FaWindows size={28}/><span>PC</span>
-        <SiPlaystation size={28}/><span>PlayStation</span>
-        <FaXbox size={28}/><span>Xbox</span>
-        <SiNintendo size={28}/><span>Nintendo</span>
+
+        <span className="text-2xl font-bold text-white">Tienda</span>
+
         <button className="bg-pink-500 p-2 rounded-full hover:bg-pink-600">
             <FaSearch size={20} />
         </button>
