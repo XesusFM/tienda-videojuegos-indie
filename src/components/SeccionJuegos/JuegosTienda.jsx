d497ff9fe088cd2882549c524f1a09f85cafe5a0
Jesus Fernandez <jesusfm1999@gmail.com>
2025-02-19 14:08:14 +0100

    ✨ feat: Añadir imagen y mensaje de estado para la sección de juegos sin disponibilidad, mejorando la comunicación con el usuario

--- src/components/SeccionJuegos/JuegosTienda.jsx
+++ src/components/SeccionJuegos/JuegosTienda.jsx
@@ -20,7 +20,15 @@ export function JuegosTienda() {
                         />
                     ))
                 ) : (
-                    <p className="text-gray-400 text-center">No hay juegos disponibles.</p>
+                    <div className="col-span-1 sm:col-span-2 lg:col-start-2 lg:col-span-1 flex flex-col items-center justify-center py-16">
+                        <img src="/nogames.png" alt="Sin juegos disponibles" />
+                        <h2 className="text-2xl font-semibold text-gray-700 mb-2">
+                            ¡Ups! No hay juegos disponibles
+                        </h2>
+                        <p className="text-gray-500 text-center max-w-md mb-4">
+                            Estamos actualizando nuestro catálogo. ¡Vuelve pronto para descubrir nuevos títulos!
+                        </p>
+                    </div>
                 )}
             </div>
         </section>
